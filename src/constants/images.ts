6f6c6bb166d7a127ffcfbbd5e0baef4c1803ff90
rlwjd31 <rlwjd31@naver.com>
2024-02-11 00:58:43 +0900

    change image file to shape

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,7 +3,29 @@ import image2 from "/assets/images/image2.jpg";
 import image3 from "/assets/images/image3.jpg";
 import image4 from "/assets/images/image4.jpg";
 import image5 from "/assets/images/image5.jpg";
+import shape1 from "/assets/images/shape1.png";
+import shape2 from "/assets/images/shape2.png";
+import shape3 from "/assets/images/shape3.png";
+import shape4 from "/assets/images/shape4.png";
+import shape5 from "/assets/images/shape5.png";
+import shape6 from "/assets/images/shape6.png";
+import shape7 from "/assets/images/shape7.png";
+import shape8 from "/assets/images/shape8.png";
+import shape9 from "/assets/images/shape9.png";
 
-const imagePaths: string[] = [image1, image2, image3, image4, image5];
+// const imagePaths: string[] = [image1, image2, image3, image4, image5];
+const imagePaths1: string[] = [image1, image2, image3, image4, image5];
+const imagePaths: string[] = [
+  shape1,
+  shape2,
+  shape3,
+  shape4,
+  shape5,
+  shape6,
+  shape7,
+  shape8,
+  shape9,
+];
 
+export { imagePaths1 };
 export default imagePaths;
