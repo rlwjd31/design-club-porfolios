490e6545aa6b8936c9f8b9eaff6d2c4b2b1de8c8
rlwjd31 <rlwjd31@naver.com>
2024-02-10 15:35:37 +0900

    fix rolling error at production

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import image1 from "../assets/images/image1.jpg";
-import image2 from "../assets/images/image2.jpg";
-import image3 from "../assets/images/image3.jpg";
-import image4 from "../assets/images/image4.jpg";
-import image5 from "../assets/images/image5.jpg";
+import image1 from "/assets/images/image1.jpg";
+import image2 from "/assets/images/image2.jpg";
+import image3 from "/assets/images/image3.jpg";
+import image4 from "/assets/images/image4.jpg";
+import image5 from "/assets/images/image5.jpg";
 import { useState } from "react";
 import useLoopImage from "../hooks/useLoopImage";
 import useMouseFollow from "../hooks/useMouseFollow";
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,20 +1,22 @@
 import { createGlobalStyle } from "styled-components";
-import NeueHaasGroteskFont from "../assets/fonts/Neue-Haas-Grotesk.ttf";
+import NeueHaasGroteskDisplayProFont from "/assets/fonts/Neue-Haas-Grotesk-Display-Pro.ttf";
 
 export const commonStyle = {
   padding: {
     main: "1.563rem",
   },
 };
-
+// body:lang(en){
+//   font-family: 'Neue Haas Grotesk Display Pro', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+// }
 // ! NeueHaasGroteskFont font를 src:url('../assets/fonts/Neue-Haas-Grotesk.ttf')로 import하면 decode fail이라고 뜬다.
 // ! 이는 vite에서 ES Module bundler를 사용하기 때문에 자원을 import하여 처리한 후 할당해주는 방식으로 해야한다.
 const GlobalStyle = createGlobalStyle`
   /* english custom fonts */
-  
   @font-face {
-    font-family: 'Neue Haas Grotesk';
-    src: url(${NeueHaasGroteskFont}) format('truetype');
+    font-family: 'Neue Haas Grotesk Display Pro';
+    src: url(${NeueHaasGroteskDisplayProFont}) format('truetype');
+    unicode-range: U+0041-005A, U+0061-007A;
   }
 
   * {
@@ -30,19 +32,24 @@ const GlobalStyle = createGlobalStyle`
   footer, header, hgroup, menu, nav, section {
     display: block;
   }
+
   body {
-    font-family: 'Noto Sans Kr', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Neue Haas Grotesk Display Pro', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     line-height: 1;
   }
-  body:lang(en){
-    font-family: 'Neue Haas Grotesk', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+
+  /* body:lang(en) {
+    font-family: 'Neue Haas Grotesk Display Pro', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  } */
+
   ol, ul {
     list-style: none;
   }
+
   blockquote, q {
     quotes: none;
   }
+
   a {
     text-decoration: none;
   }
