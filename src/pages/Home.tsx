9494981c792745dfd3c56ae60a9b517326e5cb2d
rlwjd31 <rlwjd31@naver.com>
2024-03-02 15:28:02 +0900

    constracted home page container width

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import { commonStyle } from "../styles/GlobalStyle";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  min-width: 88rem;
+  min-width: 75rem;
   height: 100%;
 `;
 
